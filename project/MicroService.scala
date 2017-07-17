b19143e6101a854796bd34e69a20a30fd6bb5768
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-07-17 16:31:50 +0100

    Updated Microservice.scala, added, scalastyle config, added plugins

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -1,21 +1,15 @@
-import sbt.Keys._
-import sbt.Tests.{SubProcess, Group}
+import sbt.Keys.{resolvers, _}
 import sbt._
 import play.routes.compiler.StaticRoutesGenerator
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
-
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+import _root_.play.sbt.routes.RoutesKeys.routesGenerator
+import uk.gov.hmrc._
+import DefaultBuildSettings._
+import uk.gov.hmrc.SbtAutoBuildPlugin
 
 trait MicroService {
-
-  import uk.gov.hmrc._
-  import DefaultBuildSettings._
-  import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt, SbtAutoBuildPlugin}
-  import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-  import uk.gov.hmrc.versioning.SbtGitVersioning
-  import play.sbt.routes.RoutesKeys.routesGenerator
-
-
-  import TestPhases._
 
   val appName: String
 
@@ -34,26 +28,14 @@ trait MicroService {
       libraryDependencies ++= appDependencies,
       retrieveManaged := true,
       evictionWarningOptions in update := EvictionWarningOptions.default.withWarnScalaVersionEviction(false),
-      routesGenerator := StaticRoutesGenerator
-    )
-    .configs(IntegrationTest)
-    .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
-    .settings(
-      Keys.fork in IntegrationTest := false,
-      unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest)(base => Seq(base / "it")),
-      addTestReportOption(IntegrationTest, "int-test-reports"),
-      testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
-      parallelExecution in IntegrationTest := false)
-      .settings(resolvers ++= Seq(
+      routesGenerator := StaticRoutesGenerator,
+      scalaVersion := "2.11.7",
+      crossScalaVersions := Seq("2.11.7"),
+      ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
+      resolvers ++= Seq(
         Resolver.bintrayRepo("hmrc", "releases"),
         Resolver.jcenterRepo
-      ))
-}
+      )
+    )
 
-private object TestPhases {
-
-  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
-    tests map {
-      test => new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
-    }
 }
