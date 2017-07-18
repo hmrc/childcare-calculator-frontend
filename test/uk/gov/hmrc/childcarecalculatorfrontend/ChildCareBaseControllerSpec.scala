c1a6ccd0f882f697707ec579ebacb2311d2abce1
kapil <kapil.sanghvi>
2017-07-18 11:53:03 +0100

    CCALC-3062 Created initial tests for controller

---
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/ChildCareBaseControllerSpec.scala
@@ -0,0 +1,16 @@
+package uk.gov.hmrc.childcarecalculatorfrontend
+
+import org.scalatest.mock.MockitoSugar
+import uk.gov.hmrc.play.test.UnitSpec
+
+class ChildCareBaseControllerSpec extends UnitSpec with MockitoSugar {
+
+  "ChildCareBaseController" should {
+    "load landing page" when {
+      "request is received" in {
+
+      }
+    }
+  }
+
+}
