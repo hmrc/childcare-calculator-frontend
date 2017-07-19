d7361272de2d2cbe57216c1f45b549235d88d03a
David Wright <davew@MacBook-Pro.local>
2017-07-19 12:36:00 +0100

    Fixed json formatters

--- app/uk/gov/hmrc/childcarecalculatorfrontend/models/Household.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/models/Household.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.childcarecalculatorfrontend.models
 
 import org.joda.time.LocalDate
@@ -7,31 +23,26 @@ import uk.gov.hmrc.childcarecalculatorfrontend.models.EmploymentStatusEnum.Emplo
 import uk.gov.hmrc.childcarecalculatorfrontend.models.LocationEnum.LocationEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.PeriodEnum.PeriodEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.TcUcBenefitsEnum.TcUcBenefitsEnum
-import uk.gov.hmrc.childcarecalculatorfrontend.models.YesNoUnsureBothEnum.{YesNoUnsureBothEnum}
+import uk.gov.hmrc.childcarecalculatorfrontend.models.YesNoUnsureBothEnum.YesNoUnsureBothEnum
 
-case class Household(
-                     tcUcBenefits: Option[TcUcBenefitsEnum] =   None,
-                     location: Option[LocationEnum] =   None,
-                     hasPartner: Boolean =   false,
-                     children: List[Child],
-                     parent: Claimant,
-                     partner: Option[Claimant]
-                    )
-object Household {
-  implicit val formatHousehold = Json.format[Household]
+//Note :- The order of these classes need to preserved to ensure json formatters are prepared in the correct order
+case class StatutoryIncome(
+                            statutoryWeeks: Double = 0.00,
+                            statutoryAmount: BigDecimal =   0.00
+                          )
+object StatutoryIncome {
+  implicit val formatStatutoryIncome = Json.format[StatutoryIncome]
 }
 
-case class Claimant(
-                    ageRange: Option[AgeRangeEnum] = None,
-                    benefits: Option[Benefits] = None,
-                    lastYearlyIncome: Option[Income]  =   None,
-                    currentYearlyIncome: Option[Income]  = None,
-                    hours: Option[BigDecimal] =   None,
-                    minimumEarnings: Option[MinimumEarnings]= None,
-                    escVouchers: Option[YesNoUnsureBothEnum] =   None
-                   )
-object Claimant {
-  implicit val formatClaimant = Json.format[Claimant]
+case class Income(
+                   employmentIncome: Option[BigDecimal] = None,
+                   pension: Option[BigDecimal] =   None,
+                   otherIncome: Option[BigDecimal] = None,
+                   benefits: Option[BigDecimal] =   None,
+                   statutoryIncome: Option[StatutoryIncome]=None
+                 )
+object Income {
+  implicit val formatIncome = Json.format[Income]
 }
 
 case class Benefits(
@@ -42,25 +53,7 @@ case class Benefits(
                    )
 object Benefits {
   implicit val formatBenefits = Json.format[Benefits]
-}
 
-case class Income(
-                  employmentIncome: Option[BigDecimal] = None,
-                  pension: Option[BigDecimal] =   None,
-                  otherIncome: Option[BigDecimal] = None,
-                  benefits: Option[BigDecimal] =   None,
-                  statutoryIncome: Option[StatutoryIncome]=None
-                 )
-object Income {
-  implicit val formatIncome = Json.format[Income]
-}
-
-case class StatutoryIncome(
-                           statutoryWeeks: Double = 0.00,
-                           statutoryAmount: BigDecimal =   0.00
-                          )
-object StatutoryIncome {
-  implicit val formatStatutoryIncome = Json.format[StatutoryIncome]
 }
 
 case class MinimumEarnings(
@@ -70,18 +63,6 @@ case class MinimumEarnings(
                           )
 object MinimumEarnings {
   implicit val formatMinimumEarnings = Json.format[MinimumEarnings]
-}
-
-case class Child(
-                 id: Short,
-                 name: String,
-                 dob: Option[LocalDate]=None,
-                 disability: Option[Disability]=None,
-                 childcareCost: Option[ChildCareCost]=None,
-                 education: Option[Education]= None
-                )
-object Child {
-  implicit val formatChild = Json.format[Child]
 }
 
 case class Disability(
@@ -108,3 +89,41 @@ case class Education(
 object Education {
   implicit val formatEducation = Json.format[Education]
 }
+
+case class Child(
+                  id: Short,
+                  name: String,
+                  dob: Option[LocalDate]=None,
+                  disability: Option[Disability]=None,
+                  childcareCost: Option[ChildCareCost]=None,
+                  education: Option[Education]= None
+                )
+object Child {
+  implicit val formatChild = Json.format[Child]
+}
+
+case class Claimant(
+                     ageRange: Option[AgeRangeEnum] = None,
+                     benefits: Option[Benefits] = None,
+                     lastYearlyIncome: Option[Income]  =   None,
+                     currentYearlyIncome: Option[Income]  = None,
+                     hours: Option[BigDecimal] =   None,
+                     minimumEarnings: Option[MinimumEarnings]= None,
+                     escVouchers: Option[YesNoUnsureBothEnum] =   None
+                   )
+object Claimant {
+  implicit val formatClaimant = Json.format[Claimant]
+}
+
+case class Household(
+                      tcUcBenefits: Option[TcUcBenefitsEnum] =   None,
+                      location: Option[LocationEnum] =   None,
+                      hasPartner: Boolean =   false,
+                      children: List[Child],
+                      parent: Claimant,
+                      partner: Option[Claimant]
+                    )
+object Household {
+  implicit val formatHousehold = Json.format[Household]
+}
+
