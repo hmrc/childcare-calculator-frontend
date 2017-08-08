c176637e4fd62002af405fec0a65d5e67c85707d
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-08-08 18:29:01 +0100

    added test cases

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/FreeHoursResultsSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/FreeHoursResultsSpec.scala
@@ -18,9 +18,9 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import play.api.data.Form
 import play.api.i18n.Messages.Implicits._
 import play.api.test.Helpers.{contentAsString, _}
+import uk.gov.hmrc.childcarecalculatorfrontend.models.LocationEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.LocationEnum.LocationEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.freeHoursResults
 import uk.gov.hmrc.childcarecalculatorfrontend.{FakeCCApplication, TemplatesValidator}
@@ -29,34 +29,71 @@ class FreeHoursResultsSpec extends TemplatesValidator with FakeCCApplication {
 
   override val contentData: List[ElementDetails] = List(
     ElementDetails(id = Some("page-title"), value = "Your eligibility for childcare support"),
-//    ElementDetails(tagName = Some("h2"), tagIndex = Some(0), value = "Still to check"),
-//    ElementDetails(tagName = Some("p"), tagIndex = Some(0), value = "By giving more information, the calculator can check to see if you’re eligible to get help from:"),
-//    ElementDetails(tagName = Some("li"), tagIndex = Some(0), value = "Childcare vouchers"),
-//    ElementDetails(tagName = Some("li"), tagIndex = Some(1), value = "Tax-Free Childcare"),
-//    ElementDetails(tagName = Some("li"), tagIndex = Some(2), value = "Tax credits"),
-    ElementDetails(id = Some("next-button"), value = "Continue"),
+    ElementDetails(id = Some("free-hours-results-eligible"), tagName=Some("h2"), tagIndex=Some(0), value = "To be eligible for other schemes"),
+    ElementDetails(id = Some("free-hours-results-eligible"), tagName=Some("h2"), tagIndex=Some(1),  value = "What you told the calculator"),
+    ElementDetails(id = Some("free-hours-results-eligible"), tagName = Some("p"), tagIndex = Some(0), value = "You " +
+      "need to have childcare costs to get support through the other schemes. So, if you’re here to see what " +
+      "childcare you can afford, you should tell the calculator that you expect to have costs. You can estimate " +
+      "how much you might pay at a later point."),
+    ElementDetails(id = Some("free-hours-results-eligible"), tagName = Some("p"), tagIndex = Some(1),
+      value = "You can go back and change your answers and see what you could get."),
+    ElementDetails(tagName = Some("th"), tagIndex = Some(0), value = "General"),
+    ElementDetails(tagName = Some("li"), tagIndex = Some(1), value = "Childcare vouchers"),
+    ElementDetails(tagName = Some("li"), tagIndex = Some(2), value = "Tax-Free Childcare"),
+    ElementDetails(tagName = Some("li"), tagIndex = Some(3), value = "Tax credits"),
     ElementDetails(id = Some("back-button"), value = "Back")
   )
 
   override val linksData: List[ElementDetails] = List(
-    ElementDetails(id = Some("back-button"), checkAttribute = Some("href"), value = expectChildcareCostsPath)
+    ElementDetails(id = Some("back-button"), checkAttribute = Some("href"), value = expectChildcareCostsPath),
+    ElementDetails(id = Some("free-hours-results-general-aged2"), checkAttribute = Some("href"), value = childAgedTwoEditPath),
+    ElementDetails(id = Some("free-hours-results-general-aged3-or-4"), checkAttribute = Some("href"), value = childAgedThreeOrFourEditPath),
+    ElementDetails(id = Some("free-hours-results-eligible-cc"), checkAttribute = Some("href"), value = expectChildcareCostsEditPath)
   )
 
-  def getTemplate(form: Form[Option[Boolean]], location: LocationEnum): Document = {
-    val template = freeHoursResults(true, location)(request, applicationMessages)
+  def getTemplate(isChild3or4: Boolean, location: LocationEnum): Document = {
+    val template = freeHoursResults(isChild3or4, location)(request, applicationMessages)
+    println(s"$template")
     Jsoup.parse(contentAsString(template))
   }
 
-//  "display correct content for 15 hours eligibility" in {
-//
-//    verifyPageContent(List(
-//          ElementDetails(tagName = Some("h2"), tagIndex = Some(0), value = "Still to check")
-//      //    ElementDetails(tagName = Some("p"), tagIndex = Some(0), value = "By giving more information, the calculator can check to see if you’re eligible to get help from:"),
-//      //    ElementDetails(tagName = Some("li"), tagIndex = Some(0), value = "Childcare vouchers"),
-//      //    ElementDetails(tagName = Some("li"), tagIndex = Some(1), value = "Tax-Free Childcare"),
-//      //    ElementDetails(tagName = Some("li"), tagIndex = Some(2), value = "Tax credits")
-//    ))
-//    verifyPageLinks()
-//
+  "render template" in {
+    val template = freeHoursResults.render(false, LocationEnum.ENGLAND, request, applicationMessages)
+    template.contentType shouldBe "text/html"
+
+    val template1 = freeHoursResults.f(true, LocationEnum.WALES)(request, applicationMessages)
+    template1.contentType shouldBe "text/html"
+  }
+
+//  "display correct content if having a child of 3 or 4 years" when {
+//    LocationEnum.values.foreach { loc =>
+//      s"${loc} is selected" in {
+//        implicit val doc: Document = getTemplate(false, loc)
+//        if (loc != "northern-ireland") {
+//          println("no NI")
+//          verifyPageContent(
+//            List(
+//            ElementDetails(id = Some("free-hours-results-not-entitled"), tagName = Some("p"), tagIndex = Some(0),
+//              value = "You’re currently not eligible for any free hours because you don’t have a child aged between " +
+//                "2 and 4. You don’t currently have or expect to have any approved childcare costs so you would not be " +
+//                "eligible for any further support.")
+//            )
+//          )
+//        } else {
+//          println("NI")
+//          verifyPageContent(
+//            List(
+//            ElementDetails(id = Some("free-hours-results-not-entitled"), tagName = Some("p"), tagIndex = Some(0),
+//              value = "You’re currently not eligible for support because you don’t have a child aged between 3 and 4. You " +
+//                "don’t currently have or expect to have any approved childcare costs so you would not be eligible for " +
+//                "any further support.")
+//            )
+//          )
+//        }
+//        verifyPageLinks()
+//      }
+//    }
 //  }
+
+
 }
