0c9a250395d82c92f4abf81b7b70f139627eaacc
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-08-22 16:45:28 +0100

    added spec for form

---
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/forms/GetBenefitsFormSpec.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.childcarecalculatorfrontend.forms
+
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.prop.Tables.Table
+import play.api.i18n.Messages.Implicits._
+import uk.gov.hmrc.childcarecalculatorfrontend.FakeCCApplication
+import uk.gov.hmrc.play.test.UnitSpec
+
+class GetBenefitsFormSpec extends UnitSpec with FakeCCApplication {
+
+  val testCases = Table(
+    ("Has partner", "Error message key"),
+    (false, "get.benefits.not.selected.error.single"),
+    (true, "get.benefits.not.selected.error.couple")
+  )
+
+  "GetBenefitsForm" when {
+    forAll(testCases) { case (hasPartner, errorMessageKey) =>
+      s"user has partner = ${hasPartner}" should {
+
+        "accept value true" in {
+          val form = new GetBenefitsForm(hasPartner, applicationMessagesApi).form.bind(
+            Map(
+              getBenefitsKey -> "true"
+            )
+          )
+          form.value.get.get shouldBe true
+          form.hasErrors shouldBe false
+          form.errors shouldBe empty
+        }
+
+        "accept value false" in {
+          val form = new GetBenefitsForm(hasPartner, applicationMessagesApi).form.bind(
+            Map(
+              getBenefitsKey -> "false"
+            )
+          )
+          form.value.get.get shouldBe false
+          form.hasErrors shouldBe false
+          form.errors shouldBe empty
+        }
+
+        s"display '${applicationMessages.messages(errorMessageKey)}' if no value supplied" in {
+          val form = new GetBenefitsForm(hasPartner, applicationMessagesApi).form.bind(
+            Map(
+              getBenefitsKey -> ""
+            )
+          )
+          form.value shouldBe None
+          form.hasErrors shouldBe true
+          form.errors.length shouldBe 1
+          form.errors.head.message shouldBe applicationMessages.messages(errorMessageKey)
+          form.errors.head.message should not be errorMessageKey
+        }
+
+        val invalidValues = List("abcd", "1234")
+        invalidValues.foreach { invalidValue =>
+          s"return error if invalid value '${invalidValue}' is supplied" in {
+            val form = new GetBenefitsForm(hasPartner, applicationMessagesApi).form.bind(
+              Map(
+                getBenefitsKey -> invalidValue
+              )
+            )
+            form.value shouldBe None
+            form.hasErrors shouldBe true
+            form.errors.length shouldBe 1
+            form.errors.head.message shouldBe "error.boolean"
+          }
+        }
+      }
+    }
+  }
+
+}
