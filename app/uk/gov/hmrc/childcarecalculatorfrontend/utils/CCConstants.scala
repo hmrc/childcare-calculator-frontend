7cc626bf0d59f9114ceca9ed2e44dbdd0dcf4dea
milena.valchanova <milena.valchanova@digital.hmrc.gov.uk>
2017-08-22 15:06:35 +0100

    Add form for 'Who gets benefits' page

--- app/uk/gov/hmrc/childcarecalculatorfrontend/utils/CCConstants.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/utils/CCConstants.scala
@@ -30,6 +30,7 @@ trait CCConstants {
   lazy val whichOfYouInPaidEmploymentKey = "whichOfYouInPaidEmployment"
   lazy val hoursKey = "hours"
   lazy val vouchersKey = "vouchers"
+  lazy val whoGetsBeneftsKey = "whoGetsBenefts"
 
   def getFamilyStatus(hasPartner: Boolean): String = {
     if(hasPartner) "couple" else "single"
