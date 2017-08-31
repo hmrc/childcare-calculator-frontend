b864398c73e2b6ff1bc5e831a4969178a9b6552b
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2017-08-31 16:00:28 +0100

    form and test created

---
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/forms/WhoGetsVouchersFormSpec.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.childcarecalculatorfrontend.forms
+
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import play.api.i18n.Messages.Implicits._
+import uk.gov.hmrc.childcarecalculatorfrontend.FakeCCApplication
+import uk.gov.hmrc.play.test.UnitSpec
+
+/**
+ * Created by user on 31/08/17.
+ */
+class WhoGetsVouchersFormSpec extends UnitSpec with FakeCCApplication {
+
+  "Who Gets vouchers form" when {
+      "user has partner" should {
+
+        "accept value true" in {
+          val form = new WhoGetsVouchersForm(applicationMessagesApi).form.bind(
+            Map(
+              whoGetsVouchersKey -> "true"
+            )
+          )
+          form.value.get.get shouldBe true
+          form.hasErrors shouldBe false
+          form.errors shouldBe empty
+        }
+
+        "accept value false" in {
+          val form = new WhoGetsVouchersForm(applicationMessagesApi).form.bind(
+            Map(
+              whoGetsVouchersKey -> "false"
+            )
+          )
+          form.value.get.get shouldBe false
+          form.hasErrors shouldBe false
+          form.errors shouldBe empty
+        }
+
+        s"display '${applicationMessages.messages("who.gets.vouchers.not.selected.error")}' if no value supplied" in {
+          val form = new WhoGetsVouchersForm(applicationMessagesApi).form.bind(
+            Map(
+              whoGetsVouchersKey -> ""
+            )
+          )
+          form.value shouldBe None
+          form.hasErrors shouldBe true
+          form.errors.length shouldBe 1
+          form.errors.head.message shouldBe applicationMessages.messages("who.gets.vouchers.not.selected.error")
+          form.errors.head.message should not be "who.gets.vouchers.not.selected.error"
+        }
+
+        val invalidValues = List("abcd", "1234")
+        invalidValues.foreach { invalidValue =>
+          s"return error if invalid value '${invalidValue}' is supplied" in {
+            val form = new WhoGetsVouchersForm(applicationMessagesApi).form.bind(
+              Map(
+                whoGetsVouchersKey -> invalidValue
+              )
+            )
+            form.value shouldBe None
+            form.hasErrors shouldBe true
+            form.errors.length shouldBe 1
+            form.errors.head.message shouldBe "error.boolean"
+          }
+        }
+      }
+  }
+
+}
