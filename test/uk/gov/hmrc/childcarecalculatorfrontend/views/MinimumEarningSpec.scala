30c86adca47120be19b82619a3a86d875b03914e
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-09-06 16:55:34 +0100

    code indented

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/MinimumEarningSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/MinimumEarningSpec.scala
@@ -25,36 +25,11 @@ import play.api.i18n.Messages.Implicits._
 import play.api.mvc.Call
 import play.api.test.Helpers._
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.MinimumEarningsForm
-import uk.gov.hmrc.childcarecalculatorfrontend.models.AgeRangeEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.utils.HelperManager
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.minimumEarning
 import uk.gov.hmrc.childcarecalculatorfrontend.{FakeCCApplication, TemplatesValidator}
 
 class MinimumEarningSpec extends TemplatesValidator with  FakeCCApplication with HelperManager {
-
-  override val config: Map[String, _] = Map(
-    "default" -> Map(
-      "apprentice" -> 56,
-      "UNDER18" -> 64,
-      "EIGHTEENTOTWENTY" -> 89,
-      "TWENTYONETOTWENTYFOUR" -> 112,
-      "OVERTWENTYFOUR" -> 120
-    ),
-    "2016/2017" -> Map(
-      "apprentice" -> 54,
-      "UNDER18" -> 64,
-      "EIGHTEENTOTWENTY" -> 88,
-      "TWENTYONETOTWENTYFOUR" -> 111,
-      "OVERTWENTYFOUR" -> 115
-    ),
-    "2017/2018" -> Map(
-      "apprentice" -> 56,
-      "UNDER18" -> 64,
-      "EIGHTEENTOTWENTY" -> 89,
-      "TWENTYONETOTWENTYFOUR" -> 112,
-      "OVERTWENTYFOUR" -> 120
-    )
-  )
 
   override val contentData: List[ElementDetails] = List(
     ElementDetails(attribute = Some("for"), attributeValue = Some("minimumEarnings-true"), value = "Yes"),
@@ -69,101 +44,89 @@ class MinimumEarningSpec extends TemplatesValidator with  FakeCCApplication with
 
   val backURL = Call("GET", whatsYourAgePath)
 
-  val ageRanges = List(
-    AgeRangeEnum.UNDER18,
-    AgeRangeEnum.EIGHTEENTOTWENTY,
-    AgeRangeEnum.TWENTYONETOTWENTYFOUR,
-    AgeRangeEnum.OVERTWENTYFOUR
+  var amount = 120.30
+
+  def getTemplate(form: Form[Option[Boolean]], isPartner: Boolean): Document = {
+    val template = minimumEarning(form, isPartner, amount, backURL)(request, applicationMessages)
+    Jsoup.parse(contentAsString(template))
+  }
+
+  val isPartnerTestCase = Table(
+    ("isPartner", "errorMessage", "pageTitle", "hintText", "submitURL"),
+    (false, "on.average.how.much.will.you.earn.parent.error", s"On average, will you earn £${amount} or more a week?", "This is the National Minimum Wage or National Living Wage a week for someone your age.", parentMinimumEarningsPath),
+    (true, "on.average.how.much.will.you.earn.partner.error", s"On average, will your partner earn £${amount} or more a week?", "This is the National Minimum Wage or National Living Wage a week for someone your partner’s age.", partnerMinimumEarningsPath)
   )
-//  "All age ranges" should {
-//    ageRanges.foreach(range => {
 
-//      val amount = getMinimumEarningsAmountForAgeRange(Some(range.toString))
-      var amount = 120.30
 
-      def getTemplate(form: Form[Option[Boolean]], isPartner: Boolean): Document = {
-        val template = minimumEarning(form, isPartner, amount, backURL)(request, applicationMessages)
-        Jsoup.parse(contentAsString(template))
+  forAll(isPartnerTestCase) { case (isPartner, errorMessage, pageTitle, hintText, submitURL) =>
+    s"calling benefits template when isPartner = ${isPartner}" should {
+      "render template" in {
+        val template = minimumEarning.render(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form, isPartner, amount, backURL, request, applicationMessages)
+        template.contentType shouldBe "text/html"
+
+        val template1 = minimumEarning.f(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form, isPartner, amount, backURL)(request, applicationMessages)
+        template1.contentType shouldBe "text/html"
       }
 
-      val isPartnerTestCase = Table(
-        ("isPartner", "errorMessage", "pageTitle", "hintText", "submitURL"),
-        (false, "on.average.how.much.will.you.earn.parent.error", s"On average, will you earn £${amount} or more a week?", "This is the National Minimum Wage or National Living Wage a week for someone your age.", parentMinimumEarningsPath),
-        (true, "on.average.how.much.will.you.earn.partner.error", s"On average, will your partner earn £${amount} or more a week?", "This is the National Minimum Wage or National Living Wage a week for someone your partner’s age.", partnerMinimumEarningsPath)
+      val dynamicContent = List(
+        ElementDetails(id = Some("page-title"), value = pageTitle),
+        ElementDetails(tagName = Some("p"), tagIndex = Some(0), value = hintText)
       )
 
+      val dynamicLinks = List(
+        ElementDetails(elementClass = Some("form"), checkAttribute = Some("action"), value = submitURL)
+      )
 
-      forAll(isPartnerTestCase) { case (isPartner, errorMessage, pageTitle, hintText, submitURL) =>
-        s"calling benefits template when isPartner = ${isPartner}" should {
-          "render template" in {
-            val template = minimumEarning.render(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form, isPartner, amount, backURL, request, applicationMessages)
-            template.contentType shouldBe "text/html"
+      "display correct content" when {
+        "nothing is selected initially" in {
+          implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(None), isPartner)
 
-            val template1 = minimumEarning.f(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form, isPartner, amount, backURL)(request, applicationMessages)
-            template1.contentType shouldBe "text/html"
-          }
+          verifyPageContent(dynamicContent)
+          verifyPageLinks(dynamicLinks)
+          verifyChecks()
+          verifyErrors()
+        }
 
-          val dynamicContent = List(
-            ElementDetails(id = Some("page-title"), value = pageTitle),
-            ElementDetails(tagName = Some("p"), tagIndex = Some(0), value = hintText)
+        "true is selected" in {
+          implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(Some(true)), isPartner)
+
+          verifyPageContent(dynamicContent)
+          verifyPageLinks(dynamicLinks)
+          verifyChecks(
+            List("minimumEarnings-true")
           )
+          verifyErrors()
+        }
 
-          val dynamicLinks = List(
-            ElementDetails(elementClass = Some("form"), checkAttribute = Some("action"), value = submitURL)
+        "false is selected" in {
+          implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(Some(false)), isPartner)
+
+          verifyPageContent(dynamicContent)
+          verifyPageLinks(dynamicLinks)
+          verifyChecks(
+            List("minimumEarnings-false")
           )
+          verifyErrors()
+        }
 
-          "display correct content" when {
-            "nothing is selected initially" in {
-              implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(None), isPartner)
+        s"display ${applicationMessages.messages(errorMessage, amount)} form is submitted without data" in {
+          val form = new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.bind(
+            Map(
+              "minimumEarnings" -> ""
+            )
+          )
+          implicit val doc: Document = getTemplate(form, isPartner)
 
-              verifyPageContent(dynamicContent)
-              verifyPageLinks(dynamicLinks)
-              verifyChecks()
-              verifyErrors()
-            }
-
-            "true is selected" in {
-              implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(Some(true)), isPartner)
-
-              verifyPageContent(dynamicContent)
-              verifyPageLinks(dynamicLinks)
-              verifyChecks(
-                List("minimumEarnings-true")
-              )
-              verifyErrors()
-            }
-
-            "false is selected" in {
-              implicit val doc: Document = getTemplate(new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.fill(Some(false)), isPartner)
-
-              verifyPageContent(dynamicContent)
-              verifyPageLinks(dynamicLinks)
-              verifyChecks(
-                List("minimumEarnings-false")
-              )
-              verifyErrors()
-            }
-
-            s"display ${applicationMessages.messages(errorMessage, amount)} form is submitted without data" in {
-              val form = new MinimumEarningsForm(isPartner, amount, applicationMessagesApi).form.bind(
-                Map(
-                  "minimumEarnings" -> ""
-                )
-              )
-              implicit val doc: Document = getTemplate(form, isPartner)
-
-              verifyPageContent(dynamicContent)
-              verifyPageLinks(dynamicLinks)
-              verifyChecks()
-              verifyErrors(
-                errors = Map("minimumEarnings" -> applicationMessages.messages(errorMessage, amount)),
-                validDateInlineErrors = false
-              )
-              applicationMessages.messages(errorMessage) should not be errorMessage
-            }
-          }
+          verifyPageContent(dynamicContent)
+          verifyPageLinks(dynamicLinks)
+          verifyChecks()
+          verifyErrors(
+            errors = Map("minimumEarnings" -> applicationMessages.messages(errorMessage, amount)),
+            validDateInlineErrors = false
+          )
+          applicationMessages.messages(errorMessage) should not be errorMessage
         }
       }
-//    })
-//  }
+    }
+  }
 }
