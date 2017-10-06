2414b4a554d415a3250b6deed9d828cef899a415
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-10-06 15:46:24 +0100

    Updated test cases

--- test/uk/gov/hmrc/childcarecalculatorfrontend/NavigatorSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/NavigatorSpec.scala
@@ -68,19 +68,92 @@ class NavigatorSpec extends SpecBase with MockitoSugar {
         navigator.nextPage(ChildAgedThreeOrFourId, NormalMode)(spy(userAnswers())) mustBe routes.ChildcareCostsController.onPageLoad(NormalMode)
       }
 
-      "go to expect approved childcare cost from childcare cost when you have childcare cost or not yet decided" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("yes") thenReturn Some("notYet")
+      "from childcare costs" when {
+        "go to expect approved childcare cost when you have childcare cost or not yet decided" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("yes") thenReturn Some("notYet")
 
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.ApprovedProviderController.onPageLoad(NormalMode)
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.ApprovedProviderController.onPageLoad(NormalMode)
-      }
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.ApprovedProviderController.onPageLoad(NormalMode)
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.ApprovedProviderController.onPageLoad(NormalMode)
+        }
 
-      "go to results page from childcare cost if you are not eligible for free hours and don't have the child care cost" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedThreeOrFour) thenReturn Some(false)
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+        "go to results page when you are not eligible for free hours and don't have the child care cost" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedThreeOrFour) thenReturn Some(false)
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+        }
+
+        "go to free hours results page when you are eligible for free hours, have child aged 3 or 4 years and no child care cost for non-england" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedThreeOrFour) thenReturn Some(true)
+          when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("northernIreland")
+          when(answers.isEligibleForFreeHours) thenReturn Eligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+        }
+
+        "go to free hours info page when you are eligible for free hours, have child aged 3 or 4 years and no child care cost for england" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedThreeOrFour) thenReturn Some(true)
+          when(answers.location) thenReturn Some("england")
+          when(answers.isEligibleForFreeHours) thenReturn Eligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
+        }
+
+        "go to results page when you are eligible for free hours, have child aged 2 and don't have the child care cost" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedTwo) thenReturn Some(true)
+          when(answers.childAgedThreeOrFour) thenReturn Some(false)
+          when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("england")
+          when(answers.isEligibleForFreeHours) thenReturn Eligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+        }
+
+        "go to free hours info for england and results page for non-england if you are eligible for free hours, have child 2 & 3 or 4 years, don't have childcare cost & lives in wales, scotland" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedTwo) thenReturn Some(true)
+          when(answers.childAgedThreeOrFour) thenReturn Some(true)
+          when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("england")
+          when(answers.isEligibleForFreeHours) thenReturn Eligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
+        }
+
+        "go to free hours info page if you are eligible for free hours, have child aged 3 or 4 years and don't have childcare cost and lives in england" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedThreeOrFour) thenReturn Some(true)
+          when(answers.location) thenReturn Some("england")
+          when(answers.isEligibleForMaxFreeHours) thenReturn Eligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
+        }
+
+        "go to free hours results page if you are eligible for free hours, have child aged 3 or 4 years and don't have childcare cost and lives in non-england" in {
+          val answers = spy(userAnswers())
+          when(answers.childcareCosts) thenReturn Some("no")
+          when(answers.childAgedThreeOrFour) thenReturn Some(true)
+          when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("northernIreland")
+          when(answers.isEligibleForMaxFreeHours) thenReturn NotEligible
+
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+          navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
+        }
+
       }
 
       "WILL YOUR CHILDCARE COSTS BE WITH AN APPROVED PROVIDER" when {
@@ -172,67 +245,6 @@ class NavigatorSpec extends SpecBase with MockitoSugar {
           when(answers.hasYourPartnersTaxCodeBeenAdjusted) thenReturn Some(false)
           navigator.nextPage(HasYourPartnersTaxCodeBeenAdjustedId, NormalMode)(answers) mustBe routes.EitherGetsVouchersController.onPageLoad(NormalMode)
         }
-      }
-
-      "go to free info page from childcare cost if you are eligible for free hours, have child aged 3 or 4 years and don't have the child care cost" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedThreeOrFour) thenReturn Some(true)
-        when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("northern-ireland")
-        when(answers.isEligibleForFreeHours) thenReturn Eligible
-
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-      }
-
-      "go to results page from childcare cost if you are eligible for free hours, have child aged 2 and don't have the child care cost" in {
-      val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedTwo) thenReturn Some(true)
-        when(answers.childAgedThreeOrFour) thenReturn Some(false)
-        when(answers.location) thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("england")
-        when(answers.isEligibleForFreeHours) thenReturn Eligible
-
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursResultController.onPageLoad()
-      }
-
-      "go to free hours info page if you are eligible for free hours, have child 2 & 3 or 4 years, don't have childcare cost & lives in wales, scotland" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedTwo) thenReturn Some(true)
-        when(answers.childAgedThreeOrFour) thenReturn Some(true)
-        when(answers.location) thenReturn Some("wales") thenReturn Some("scotland")
-        when(answers.isEligibleForFreeHours) thenReturn Eligible
-
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-      }
-
-      "go to free hours info page if you are eligible for free hours, have child aged 3 or 4 years and don't have childcare cost and lives in england" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedThreeOrFour) thenReturn Some(true)
-        when(answers.location) thenReturn Some("england") thenReturn Some("wales") thenReturn Some("scotland") thenReturn Some("northernIreland")
-        when(answers.isEligibleForFreeHours) thenReturn Eligible
-
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
-      }
-
-      "go to free hours info page if you are eligible for free hours, have child 2 and 3 or 4 years, don't have childcare cost and lives in england" in {
-        val answers = spy(userAnswers())
-        when(answers.childcareCosts) thenReturn Some("no")
-        when(answers.childAgedTwo) thenReturn Some(true)
-        when(answers.childAgedThreeOrFour) thenReturn Some(true)
-        when(answers.location) thenReturn Some("england")
-        when(answers.isEligibleForFreeHours) thenReturn Eligible
-
-        navigator.nextPage(ChildcareCostsId, NormalMode)(answers) mustBe routes.FreeHoursInfoController.onPageLoad()
       }
 
       "go to are you in paid work from do you live with partner when user selects No" in {
