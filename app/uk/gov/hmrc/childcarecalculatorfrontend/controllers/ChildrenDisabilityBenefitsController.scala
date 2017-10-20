0fff1e805e683cbbb466be074978a01ba632ad84
anujakhare <anuja.khare@digital.hmrc.gov.uk>
2017-10-20 13:47:17 +0100

    CCALC-3414-Single:Applied validation

--- app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/ChildrenDisabilityBenefitsController.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/ChildrenDisabilityBenefitsController.scala
@@ -50,7 +50,7 @@ class ChildrenDisabilityBenefitsController @Inject()(appConfig: FrontendAppConfi
 
   def onSubmit(mode: Mode) = (getData andThen requireData).async {
     implicit request =>
-      BooleanForm().bindFromRequest().fold(
+      BooleanForm("childrenDisabilityBenefits.error").bindFromRequest().fold(
         (formWithErrors: Form[Boolean]) =>
           Future.successful(BadRequest(childrenDisabilityBenefits(appConfig, formWithErrors, mode))),
         (value) =>
