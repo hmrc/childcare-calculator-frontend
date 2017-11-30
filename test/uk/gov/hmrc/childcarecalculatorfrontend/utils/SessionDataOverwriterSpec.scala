5079cb72e9d53fbfa6c39efbf91edb0a4a29bd19
juan <juanan_rp@hotmail.co.uk>
2017-11-30 11:55:53 +0000

    refactoring

--- test/uk/gov/hmrc/childcarecalculatorfrontend/utils/SessionDataOverwriterSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/utils/SessionDataOverwriterSpec.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.childcarecalculatorfrontend.utils
 
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.{Format, JsNumber, JsString}
-import uk.gov.hmrc.childcarecalculatorfrontend.identifiers.ChildcareCostsId
+import uk.gov.hmrc.childcarecalculatorfrontend.identifiers._
 import uk.gov.hmrc.childcarecalculatorfrontend.models.YesNoNotYetEnum.YesNoNotYetEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.YesNoUnsureEnum.YesNoUnsureEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.{YesNoNotYetEnum, YesNoUnsureEnum}
@@ -30,7 +30,7 @@ class SessionDataOverwriterSpec extends PlaySpec {
     "overwrite value" when {
       "the value exists in CacheMap and matches criteria" in {
         val data = new CacheMap("id", Map(ChildcareCostsId.toString -> JsString(YesNoNotYetEnum.NOTYET.toString)))
-        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data,ChildcareCostsId.toString,YesNoNotYetEnum.NOTYET,YesNoNotYetEnum.YES)
+        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data, ChildcareCostsId.toString, YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
         result.data(ChildcareCostsId.toString).as[YesNoNotYetEnum] mustBe YesNoNotYetEnum.YES
       }
     }
@@ -38,46 +38,71 @@ class SessionDataOverwriterSpec extends PlaySpec {
     "not overwrite anything" when {
       "the value exists in CacheMap but does not match our criteria" in {
         val data = new CacheMap("id", Map(ChildcareCostsId.toString -> JsString(YesNoNotYetEnum.YES.toString)))
-        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data,ChildcareCostsId.toString,YesNoNotYetEnum.NOTYET,YesNoNotYetEnum.YES)
+        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data, ChildcareCostsId.toString, YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
         result.data(ChildcareCostsId.toString).as[YesNoNotYetEnum] mustBe YesNoNotYetEnum.YES
       }
 
       "the value does not exist in our CacheMap" in {
         val data = new CacheMap("id", Map("test" -> JsString(YesNoNotYetEnum.NOTYET.toString)))
-        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data,ChildcareCostsId.toString,YesNoNotYetEnum.NOTYET,YesNoNotYetEnum.YES)
+        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data, ChildcareCostsId.toString, YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
         result.data.get(ChildcareCostsId.toString) mustBe None
       }
 
       "there is other values but not the one we need" in {
         val data = new CacheMap("id", Map(ChildcareCostsId.toString -> JsString(YesNoNotYetEnum.NOTYET.toString), "test" -> JsNumber(5)))
-        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data,ChildcareCostsId.toString,YesNoNotYetEnum.NOTYET,YesNoNotYetEnum.YES)
+        val result = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data, ChildcareCostsId.toString, YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
         result.data("test").as[Int] mustBe 5
       }
     }
 
     "do multiple overwrites" when {
       "we have two elements to overwrite" in {
-        val data = new CacheMap("id", Map("test1" -> JsString(YesNoNotYetEnum.NOTYET.toString),"test2" -> JsString(YesNoUnsureEnum.NOTSURE.toString)))
+        val data = new CacheMap("id", Map("test1" -> JsString(YesNoNotYetEnum.NOTYET.toString), "test2" -> JsString(YesNoUnsureEnum.NOTSURE.toString)))
 
-        val update = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data,"test1",YesNoNotYetEnum.NOTYET,YesNoNotYetEnum.YES)
-        val update2 = SessionDataOverwrite.overwrite[YesNoUnsureEnum](update,"test2",YesNoUnsureEnum.NOTSURE,YesNoUnsureEnum.NO)
+        val update = SessionDataOverwrite.overwrite[YesNoNotYetEnum](data, "test1", YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
+        val update2 = SessionDataOverwrite.overwrite[YesNoUnsureEnum](update, "test2", YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.NO)
 
         update2.data("test1").as[YesNoNotYetEnum] mustBe YesNoNotYetEnum.YES
         update2.data("test2").as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.NO
+      }
+
+      "we have all required business elements to overwrite" in {
+        val data = new CacheMap("id", Map(ChildcareCostsId.toString -> JsString(YesNoNotYetEnum.NOTYET.toString),
+          ApprovedProviderId.toString -> JsString(YesNoUnsureEnum.NOTSURE.toString),
+          DoYouKnowYourAdjustedTaxCodeId.toString -> JsString(YesNoUnsureEnum.NOTSURE.toString),
+          DoYouKnowYourPartnersAdjustedTaxCodeId.toString -> JsString(YesNoUnsureEnum.NOTSURE.toString),
+          PartnerChildcareVouchersId.toString -> JsString(YesNoUnsureEnum.NOTSURE.toString),
+          YourChildcareVouchersId.toString -> JsString(YesNoUnsureEnum.NOTSURE.toString)))
+
+        val result = SessionDataOverwrite.applyRules(data)
+
+        result.data(ChildcareCostsId.toString).as[YesNoNotYetEnum] mustBe YesNoNotYetEnum.YES
+        result.data(ApprovedProviderId.toString).as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.YES
+        result.data(DoYouKnowYourAdjustedTaxCodeId.toString).as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.NO
+        result.data(DoYouKnowYourPartnersAdjustedTaxCodeId.toString).as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.NO
+        result.data(PartnerChildcareVouchersId.toString).as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.YES
+        result.data(YourChildcareVouchersId.toString).as[YesNoUnsureEnum] mustBe YesNoUnsureEnum.YES
       }
     }
   }
 
   object SessionDataOverwrite extends SubCascadeUpsert {
+    def applyRules(userData: CacheMap) : CacheMap = {
+      val childCareCostsIfNotYetThenYes = SessionDataOverwrite.overwrite[YesNoNotYetEnum](_:CacheMap, ChildcareCostsId.toString, YesNoNotYetEnum.NOTYET, YesNoNotYetEnum.YES)
+      val approvedProviderIfNotSureThenYes = SessionDataOverwrite.overwrite[YesNoUnsureEnum](_:CacheMap, ApprovedProviderId.toString, YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.YES)
+      val yourAdjustedTaxCodeIfNotSureThenNo = SessionDataOverwrite.overwrite[YesNoUnsureEnum](_:CacheMap, DoYouKnowYourAdjustedTaxCodeId.toString, YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.NO)
+      val partnerAdjustedTaxCodeIfNotSureThenNo = SessionDataOverwrite.overwrite[YesNoUnsureEnum](_:CacheMap, DoYouKnowYourPartnersAdjustedTaxCodeId.toString, YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.NO)
+      val partnerChildcarePartnerIfNotSureThenNo = SessionDataOverwrite.overwrite[YesNoUnsureEnum](_:CacheMap, PartnerChildcareVouchersId.toString, YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.YES)
+      val yourChildcareVouchersIfNotSureThenNo = SessionDataOverwrite.overwrite[YesNoUnsureEnum](_:CacheMap, YourChildcareVouchersId.toString, YesNoUnsureEnum.NOTSURE, YesNoUnsureEnum.YES)
+
+      (childCareCostsIfNotYetThenYes andThen  approvedProviderIfNotSureThenYes andThen yourAdjustedTaxCodeIfNotSureThenNo andThen partnerAdjustedTaxCodeIfNotSureThenNo andThen partnerChildcarePartnerIfNotSureThenNo andThen yourChildcareVouchersIfNotSureThenNo)(userData)
+    }
+
     def overwrite[T: Format](userSelections: CacheMap, elementToOverwrite: String, valueToReplace: T, valueToReplaceWith: T): CacheMap = {
       userSelections.data.get(elementToOverwrite).fold(userSelections)(itemToOverwrite => {
-        if (itemToOverwrite.as[T](implicitly[Format[T]]) == valueToReplace) {
-          store(elementToOverwrite, valueToReplaceWith, userSelections)
-        }
-        else {
-          userSelections
-        }
+        if (itemToOverwrite.as[T] == valueToReplace) store(elementToOverwrite, valueToReplaceWith, userSelections) else userSelections
       })
     }
   }
+
 }
