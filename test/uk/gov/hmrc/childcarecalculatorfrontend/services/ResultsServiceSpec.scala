6ba0d08cbb717b4a9903b1826d3244245bfbf5fb
juan <juanan_rp@hotmail.co.uk>
2017-12-11 13:11:58 +0000

    starting work for getting TC value

---
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/services/ResultsServiceSpec.scala
@@ -0,0 +1,81 @@
+package uk.gov.hmrc.childcarecalculatorfrontend.services
+
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.JsValue
+import play.api.mvc.Request
+import uk.gov.hmrc.childcarecalculatorfrontend.models.{Scheme, SchemeEnum, SchemeResults, TaxCreditsEligibility}
+import uk.gov.hmrc.childcarecalculatorfrontend.utils.UserAnswers
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.cache.client.CacheMap
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+class ResultsServiceSpec extends PlaySpec with MockitoSugar {
+
+  implicit val hc = HeaderCarrier()
+  implicit val req: Request[_] = mock[Request[_]]
+  def userAnswers(answers: (String, JsValue)*): UserAnswers = new UserAnswers(CacheMap("", Map(answers: _*)))
+
+  "Result Service" must {
+    "Return TC values" when {
+      "It is eligible" in {
+        val scheme = Scheme(name = SchemeEnum.TCELIGIBILITY,500,None,Some(TaxCreditsEligibility(true,true)))
+        val schemeResults = SchemeResults(List(scheme))
+        val answers = spy(userAnswers())
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+
+        val resultService = new ResultsService(eligibilityService, answers)
+        val values = Await.result(resultService.getTCValues(),Duration.Inf)
+
+        values.get mustBe 500
+      }
+    }
+
+    "Return None" when {
+      "It is not eligible for TC scheme" in {
+        val scheme = Scheme(name = SchemeEnum.TCELIGIBILITY,0,None,Some(TaxCreditsEligibility(true,true)))
+        val schemeResults = SchemeResults(List(scheme))
+        val answers = spy(userAnswers())
+
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+
+        val resultService = new ResultsService(eligibilityService,answers)
+        val values = Await.result(resultService.getTCValues(),Duration.Inf)
+
+        values mustBe empty
+      }
+    }
+  }
+
+  val eligibilityService = mock[EligibilityService]
+
+  class ResultsService(eligibilityService: EligibilityService, answers: UserAnswers) {
+    def getTCValues()(implicit req: play.api.mvc.Request[_], hc: HeaderCarrier) : Future[Option[BigDecimal]] = {
+
+      val result = eligibilityService.eligibility(answers)
+
+      result.map(results => {
+        val scheme: Option[Scheme] = results.schemes.find(scheme=> scheme.name == SchemeEnum.TCELIGIBILITY)
+        scheme match {
+          case Some(scheme) => {
+            if (scheme.amount > 0) {
+              Some(scheme.amount)
+            }
+            else{
+              None
+            }
+          }
+          case _ => None
+        }
+      })
+    }
+  }
+}
+
+
