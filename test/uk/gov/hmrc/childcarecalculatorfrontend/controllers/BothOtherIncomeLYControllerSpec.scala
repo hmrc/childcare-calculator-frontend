31d290021b50330cd23a13ced4008a54aa58b3ee
Nick Wilson <thenickwilson@gmail.com>
2017-12-18 14:22:07 +0000

    Dynamic taxc years on both other income LY page

--- test/uk/gov/hmrc/childcarecalculatorfrontend/controllers/BothOtherIncomeLYControllerSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/controllers/BothOtherIncomeLYControllerSpec.scala
@@ -26,17 +26,20 @@ import play.api.test.Helpers._
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.BooleanForm
 import uk.gov.hmrc.childcarecalculatorfrontend.identifiers.BothOtherIncomeLYId
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
+import uk.gov.hmrc.childcarecalculatorfrontend.utils.TaxYearInfo
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.bothOtherIncomeLY
 
 class BothOtherIncomeLYControllerSpec extends ControllerSpecBase {
+
+  val taxYearInfo = new TaxYearInfo
 
   def onwardRoute = routes.WhatToTellTheCalculatorController.onPageLoad()
 
   def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
     new BothOtherIncomeLYController(frontendAppConfig, messagesApi, FakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute),
-      dataRetrievalAction, new DataRequiredActionImpl)
+      dataRetrievalAction, new DataRequiredActionImpl, taxYearInfo)
 
-  def viewAsString(form: Form[Boolean] = BooleanForm()) = bothOtherIncomeLY(frontendAppConfig, form, NormalMode)(fakeRequest, messages).toString
+  def viewAsString(form: Form[Boolean] = BooleanForm()) = bothOtherIncomeLY(frontendAppConfig, form, NormalMode, taxYearInfo)(fakeRequest, messages).toString
 
   "BothOtherIncomeLY Controller" must {
 
