f51dfcec88c92d34841af608483b6c22cfa59454
Vineet Tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2017-12-20 16:08:44 +0000

    CCALC-3715 Add tests for free hours dynamic text

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/ResultEligibleViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/ResultEligibleViewSpec.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.childcarecalculatorfrontend.views
 
+import uk.gov.hmrc.childcarecalculatorfrontend.models.Location
 import uk.gov.hmrc.childcarecalculatorfrontend.models.views.ResultsViewModel
 import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.ViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.resultEligible
@@ -57,6 +58,70 @@ class ResultEligibleViewSpec extends ViewBehaviours {
         assertContainsMessages(view, "900")
       }
 
+    }
+
+    "display correct no of free hours and text when user is eligible for free hours" when {
+      "location is England and no of hours is 15" in {
+        val model = ResultsViewModel(freeHours = Some(15), location = Some(Location.ENGLAND))
+        val view = asDocument(resultEligible(model)(fakeRequest, messages))
+
+        assertContainsMessages(view, "15")
+        assertContainsText(view, messages("result.free.hours.title"))
+        assertContainsText(view, messages("result.free.hours.hours"))
+        assertContainsText(view, messages("result.free.hours.period.england", 570))
+        assertContainsText(view, messages("result.free.hours.para1"))
+        assertContainsText(view, messages("result.free.hours.para2"))
+      }
+
+      "location is England and no of hours is 30" in {
+        val model = ResultsViewModel(freeHours = Some(30), location = Some(Location.ENGLAND))
+        val view = asDocument(resultEligible(model)(fakeRequest, messages))
+
+        assertContainsMessages(view, "30")
+        assertContainsText(view, messages("result.free.hours.title"))
+        assertContainsText(view, messages("result.free.hours.hours"))
+        assertContainsText(view, messages("result.free.hours.period.england", 1140))
+        assertContainsText(view, messages("result.free.hours.para1"))
+        assertContainsText(view, messages("result.free.hours.para2"))
+
+      }
+
+      "location is Wales" in {
+        val model = ResultsViewModel(freeHours = Some(10), location = Some(Location.WALES))
+        val view = asDocument(resultEligible(model)(fakeRequest, messages))
+
+        assertContainsMessages(view, "10")
+        assertContainsText(view, messages("result.free.hours.title"))
+        assertContainsText(view, messages("result.free.hours.hours"))
+        assertContainsText(view, messages("result.free.hours.period.wales"))
+        assertContainsText(view, messages("result.free.hours.para1"))
+        assertContainsText(view, messages("result.free.hours.para2"))
+
+      }
+
+      "location is Scotland" in {
+        val model = ResultsViewModel(freeHours = Some(16), location = Some(Location.SCOTLAND))
+        val view = asDocument(resultEligible(model)(fakeRequest, messages))
+
+        assertContainsMessages(view, "16")
+        assertContainsText(view, messages("result.free.hours.title"))
+        assertContainsText(view, messages("result.free.hours.hours"))
+        assertContainsText(view, messages("result.free.hours.period.scotland"))
+        assertContainsText(view, messages("result.free.hours.para1"))
+        assertContainsText(view, messages("result.free.hours.para2"))
+      }
+
+      "location is NI" in {
+        val model = ResultsViewModel(freeHours = Some(12.5), location = Some(Location.NORTHERN_IRELAND))
+        val view = asDocument(resultEligible(model)(fakeRequest, messages))
+
+        assertContainsMessages(view, "12 and a half hours")
+        assertContainsText(view, messages("result.free.hours.title"))
+        assertContainsText(view, messages("result.free.hours.hours"))
+        assertContainsText(view, messages("result.free.hours.period.northern-ireland"))
+        assertContainsText(view, messages("result.free.hours.para1"))
+        assertContainsText(view, messages("result.free.hours.para2"))
+      }
     }
   }
 }
