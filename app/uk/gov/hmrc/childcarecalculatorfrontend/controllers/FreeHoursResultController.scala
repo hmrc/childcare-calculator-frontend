0269b28caae61b0de25e2c5c16ded23fa3e84fca
juan <juanan_rp@hotmail.co.uk>
2017-12-22 15:34:10 +0000

    starting adding logic to hide or show work paragraphs

--- app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/FreeHoursResultController.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/FreeHoursResultController.scala
@@ -22,7 +22,7 @@ import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.childcarecalculatorfrontend.FrontendAppConfig
 import uk.gov.hmrc.childcarecalculatorfrontend.controllers.actions.{DataRequiredAction, DataRetrievalAction}
 import uk.gov.hmrc.childcarecalculatorfrontend.models.schemes.FreeHours
-import uk.gov.hmrc.childcarecalculatorfrontend.utils.{CheckYourAnswersHelper, Utils}
+import uk.gov.hmrc.childcarecalculatorfrontend.utils.{CheckYourAnswersHelper, ChildcareConstants, Utils}
 import uk.gov.hmrc.childcarecalculatorfrontend.viewmodels.AnswerSection
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.freeHoursResult
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
@@ -48,6 +48,16 @@ class FreeHoursResultController @Inject()(appConfig: FrontendAppConfig,
         checkYourAnswersHelper.childcareCosts
       ).flatten))
 
-    Ok(freeHoursResult(appConfig, location, eligibility, sections))
+
+      val paidEmployment = if (request.userAnswers.areYouInPaidWork.isDefined) {
+        request.userAnswers.areYouInPaidWork.getOrElse(false)
+      }else{
+        request.userAnswers.whoIsInPaidEmployment.fold(true)(whoInPaidEmployment => whoInPaidEmployment match {
+          case ChildcareConstants.neither => false
+          case _ => true
+        })
+      }
+
+    Ok(freeHoursResult(appConfig, location, eligibility, sections,paidEmployment))
   }
 }
