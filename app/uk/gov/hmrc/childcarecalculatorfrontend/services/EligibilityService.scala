a167a673c21822cb4850c2d9ae2c0d3d9188f590
juan <juanan_rp@hotmail.co.uk>
2018-01-15 15:48:06 +0000

    removing logger

--- app/uk/gov/hmrc/childcarecalculatorfrontend/services/EligibilityService.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/services/EligibilityService.scala
@@ -42,9 +42,7 @@ class EligibilityService @Inject()(appConfig: FrontendAppConfig, utils: Utils, t
   def eligibility(answers: UserAnswers)(implicit req: play.api.mvc.Request[_], hc: HeaderCarrier): Future[SchemeResults] = {
 
     val household = userAnswerToHousehold.convert(answers)
-
-    Logger.warn(s"Json body: ${Json.toJson(household)}")
-
+    
     connector.getEligibility(household).map {
       results => {
         results
