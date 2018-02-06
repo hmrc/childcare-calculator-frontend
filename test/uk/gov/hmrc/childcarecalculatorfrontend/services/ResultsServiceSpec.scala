cf16447790a1db34c141b0e67ed824f90f38b51a
juan <juanan_rp@hotmail.co.uk>
2018-02-06 15:24:34 +0000

    adding functionality for warning messages on TFC

--- test/uk/gov/hmrc/childcarecalculatorfrontend/services/ResultsServiceSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/services/ResultsServiceSpec.scala
@@ -282,6 +282,75 @@ class ResultsServiceSpec extends PlaySpec with MockitoSugar with SpecBase {
 
         values.freeHours mustBe None
       }
+
+      "User eligible for tc and not tfc" in {
+        val tcScheme = Scheme(name = SchemeEnum.TCELIGIBILITY, 500, None, Some(TaxCreditsEligibility(true, true)))
+        val tfcScheme = Scheme(name = SchemeEnum.TFCELIGIBILITY, 500, None, None)
+        val escScheme = Scheme(name = SchemeEnum.ESCELIGIBILITY, 600, Some(EscClaimantEligibility(true, true)), None)
+        val schemeResults = SchemeResults(List(tcScheme, tfcScheme, escScheme))
+        val answers = spy(userAnswers())
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+
+        val resultService = new ResultsService(eligibilityService,freeHours, maxFreeHours,firstParagraphBuilder)
+        val values = Await.result(resultService.getResultsViewModel(answers), Duration.Inf)
+
+        values.showTFCWarning mustBe true
+        values.tfcWarningMessage mustBe messages("result.schemes.tfc.tc.warning")
+      }
+    }
+
+    "Return View Model with no TFC warning message" when {
+      "They are eligible for TFC and not TC" in {
+        val tcScheme = Scheme(name = SchemeEnum.TCELIGIBILITY, 800, None, Some(TaxCreditsEligibility(true, true)))
+        val tfcScheme = Scheme(name = SchemeEnum.TFCELIGIBILITY, 0, None, None)
+        val escScheme = Scheme(name = SchemeEnum.ESCELIGIBILITY, 600, Some(EscClaimantEligibility(true, true)), None)
+        val schemeResults = SchemeResults(List(tcScheme, tfcScheme, escScheme))
+        val answers = spy(userAnswers())
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+        when(answers.taxOrUniversalCredits) thenReturn Some("uc")
+
+        val resultService = new ResultsService(eligibilityService,freeHours, maxFreeHours,firstParagraphBuilder)
+        val values = Await.result(resultService.getResultsViewModel(answers), Duration.Inf)
+
+        values.showTFCWarning mustBe false
+      }
+    }
+
+    "Return View Model with TFC warning message" when {
+      "They are eligible for TC and TFC" in {
+        val tcScheme = Scheme(name = SchemeEnum.TCELIGIBILITY, 500, None, Some(TaxCreditsEligibility(true, true)))
+        val tfcScheme = Scheme(name = SchemeEnum.TFCELIGIBILITY, 500, None, None)
+        val escScheme = Scheme(name = SchemeEnum.ESCELIGIBILITY, 600, Some(EscClaimantEligibility(true, true)), None)
+        val schemeResults = SchemeResults(List(tcScheme, tfcScheme, escScheme))
+        val answers = spy(userAnswers())
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+
+        val resultService = new ResultsService(eligibilityService,freeHours, maxFreeHours,firstParagraphBuilder)
+        val values = Await.result(resultService.getResultsViewModel(answers), Duration.Inf)
+
+        values.showTFCWarning mustBe true
+        values.tfcWarningMessage mustBe messages("result.schemes.tfc.tc.warning")
+      }
+
+      "They are eligible for TFC and have UC" in {
+        val tcScheme = Scheme(name = SchemeEnum.TCELIGIBILITY, 0, None, Some(TaxCreditsEligibility(true, true)))
+        val tfcScheme = Scheme(name = SchemeEnum.TFCELIGIBILITY, 500, None, None)
+        val escScheme = Scheme(name = SchemeEnum.ESCELIGIBILITY, 600, Some(EscClaimantEligibility(true, true)), None)
+        val schemeResults = SchemeResults(List(tcScheme, tfcScheme, escScheme))
+        val answers = spy(userAnswers())
+
+        when(eligibilityService.eligibility(any())(any(), any())) thenReturn Future.successful(schemeResults)
+        when(answers.taxOrUniversalCredits) thenReturn Some("uc")
+
+        val resultService = new ResultsService(eligibilityService,freeHours, maxFreeHours,firstParagraphBuilder)
+        val values = Await.result(resultService.getResultsViewModel(answers), Duration.Inf)
+
+        values.showTFCWarning mustBe true
+        values.tfcWarningMessage mustBe messages("result.schemes.tfc.uc.warning")
+      }
     }
   }
 
