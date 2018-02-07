bd0a851a2ec4f80a14cc3fcd6ec81bfd6414161b
Vineet Tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2018-02-07 16:15:14 +0000

    CCALC-3974 Add test and code refactor

--- test/uk/gov/hmrc/childcarecalculatorfrontend/models/schemes/tfc/ModelFactorySpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/models/schemes/tfc/ModelFactorySpec.scala
@@ -18,8 +18,10 @@ package uk.gov.hmrc.childcarecalculatorfrontend.models.schemes.tfc
 
 import org.mockito.Mockito._
 import org.scalatest.{MustMatchers, OptionValues}
+import uk.gov.hmrc.childcarecalculatorfrontend.models.SelfEmployedOrApprenticeOrNeitherEnum
 import uk.gov.hmrc.childcarecalculatorfrontend.models.WhichBenefitsEnum._
 import uk.gov.hmrc.childcarecalculatorfrontend.models.schemes.SchemeSpec
+import uk.gov.hmrc.childcarecalculatorfrontend.utils.ChildcareConstants._
 
 class ModelFactorySpec extends SchemeSpec with MustMatchers with OptionValues {
   
@@ -42,11 +44,12 @@ class ModelFactorySpec extends SchemeSpec with MustMatchers with OptionValues {
         val answers = spy(helper())
         when(answers.doYouLiveWithPartner) thenReturn Some(false)
         when(answers.areYouInPaidWork) thenReturn Some(true)
-        when(answers.parentWorkHours) thenReturn Some(BigDecimal(110))
         when(answers.doYouGetAnyBenefits) thenReturn Some(false)
-        when(answers.yourMinimumEarnings) thenReturn Some(true)
+        when(answers.yourMinimumEarnings) thenReturn Some(false)
         when(answers.yourMaximumEarnings) thenReturn Some(false)
-        factory(answers).value mustEqual SingleHousehold(Parent(minEarnings = true, maxEarnings = true,
+        when(answers.yourSelfEmployed) thenReturn Some(true)
+        when(answers.areYouSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString)
+        factory(answers).value mustEqual SingleHousehold(Parent(minEarnings = false, maxEarnings = true,
                                                                 selfEmployed = true, apprentice = false, Set.empty))
       }
 
@@ -76,21 +79,66 @@ class ModelFactorySpec extends SchemeSpec with MustMatchers with OptionValues {
     "partner user" when {
 
       "return `Some` when all data is available" in {
-       /* val answers = spy(helper())
+        val answers = spy(helper())
         when(answers.doYouLiveWithPartner) thenReturn Some(true)
-        when(answers.whoIsInPaidEmployment) thenReturn Some(YouPartnerBothEnum.BOTH.toString)
-        when(answers.parentWorkHours) thenReturn Some(BigDecimal(110))
-        when(answers.partnerWorkHours) thenReturn Some(BigDecimal(120))
-        when(answers.doYouOrYourPartnerGetAnyBenefits) thenReturn Some(true)
-        when(answers.whoGetsBenefits) thenReturn Some(YouPartnerBothEnum.BOTH.toString)
-        when(answers.whichBenefitsYouGet) thenReturn Some(Set(DISABILITYBENEFITS.toString))
-        when(answers.whichBenefitsPartnerGet) thenReturn Some(Set(CARERSALLOWANCE.toString))
+        when(answers.whoIsInPaidEmployment) thenReturn Some(Both)
+        when(answers.doYouGetAnyBenefits) thenReturn Some(false)
+        when(answers.yourMinimumEarnings) thenReturn Some(false)
+        when(answers.partnerMinimumEarnings) thenReturn Some(false)
+
+        when(answers.yourSelfEmployed) thenReturn Some(true)
+        when(answers.areYouSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString)
+
+        when(answers.partnerSelfEmployed) thenReturn Some(true)
+        when(answers.partnerSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString)
+
+        when(answers.doYouOrYourPartnerGetAnyBenefits) thenReturn Some(false)
+
         factory(answers).value mustEqual JointHousehold(
-          Parent(110, Set(DISABILITYBENEFITS)),
-          Parent(120, Set(CARERSALLOWANCE))
-        )*/
+          Parent(minEarnings = false, maxEarnings = true, selfEmployed = true, apprentice = false, Set.empty),
+          Parent(minEarnings = false, maxEarnings = true, selfEmployed = true, apprentice = false, Set.empty)
+        )
       }
 
+      "return None when whoIsInPaidEmployment value is not defined" in {
+        val answers = spy(helper())
+
+        when(answers.doYouLiveWithPartner) thenReturn Some(true)
+        when(answers.doYouGetAnyBenefits) thenReturn Some(false)
+        when(answers.yourMinimumEarnings) thenReturn Some(false)
+        when(answers.partnerMinimumEarnings) thenReturn Some(false)
+
+        when(answers.yourSelfEmployed) thenReturn Some(true)
+        when(answers.areYouSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString)
+
+        when(answers.partnerSelfEmployed) thenReturn Some(true)
+        when(answers.partnerSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString)
+
+        when(answers.doYouOrYourPartnerGetAnyBenefits) thenReturn Some(false)
+
+        factory(answers) mustNot be(defined)
+      }
+
+      "return Some when Parent is Apprentice and Partner satisfies the min amd max earnings" in {
+        val answers = spy(helper())
+
+        when(answers.doYouLiveWithPartner) thenReturn Some(true)
+        when(answers.whoIsInPaidEmployment) thenReturn Some(Both)
+        when(answers.doYouGetAnyBenefits) thenReturn Some(false)
+        when(answers.yourMinimumEarnings) thenReturn Some(false)
+        when(answers.partnerMinimumEarnings) thenReturn Some(true)
+        when(answers.partnerMaximumEarnings) thenReturn Some(false)
+
+        when(answers.yourSelfEmployed) thenReturn Some(true)
+        when(answers.areYouSelfEmployedOrApprentice) thenReturn Some(SelfEmployedOrApprenticeOrNeitherEnum.APPRENTICE.toString)
+
+        when(answers.doYouOrYourPartnerGetAnyBenefits) thenReturn Some(false)
+
+        factory(answers).value mustEqual JointHousehold(
+          Parent(minEarnings = false, maxEarnings = true, selfEmployed = false, apprentice = true, Set.empty),
+          Parent(minEarnings = true, maxEarnings = true, selfEmployed = false, apprentice = false, Set.empty)
+        )
+      }
 
     }
   }
