d22456d86acd4bfa76b3631d3781dc37f89ddd7a
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-02-16 12:45:38 +0000

    controller update

--- app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/SessionManagementController.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/controllers/SessionManagementController.scala
@@ -21,15 +21,43 @@ import javax.inject.Inject
 
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.Action
+import uk.gov.hmrc.childcarecalculatorfrontend.connectors.DataCacheConnector
+import uk.gov.hmrc.childcarecalculatorfrontend.controllers.actions.DataRetrievalAction
+import uk.gov.hmrc.childcarecalculatorfrontend.utils.{UserAnswers, Utils}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import uk.gov.hmrc.childcarecalculatorfrontend.{FrontendAppConfig}
 
 import scala.concurrent.Future
 
 class SessionManagementController @Inject()(val appConfig: FrontendAppConfig,
-                                            val messagesApi: MessagesApi) extends FrontendController with I18nSupport {
+                                            getData: DataRetrievalAction,
+                                            dataCacheConnector: DataCacheConnector,
+                                            val messagesApi: MessagesApi,
+                                             utils: Utils) extends FrontendController with I18nSupport {
 
   def sessionExtend = Action.async {
     Future.successful(Ok("OK"))
   }
+
+  def sessionClearData = Action.async {
+    Future.successful(Ok("OK"))
+  }
+//  def sessionClearData = getData.async {
+//    implicit request =>
+//     request.userAnswers.map{
+//        x => {
+//            dataCacheConnector.save[Location.Value](request.sessionId, LocationId.toString, x).map(cacheMap =>
+//            Redirect(navigator.nextPage(LocationId, mode)(new UserAnswers(cacheMap))))
+//        }
+//      }
+//  }
 }
+
+//    implicit request =>
+//        (formWithErrors: Form[_]) =>
+//          Future.successful(BadRequest(location(appConfig, formWithErrors, mode))),
+//        (value) =>
+//          dataCacheConnector.save[Location.Value](request.sessionId, LocationId.toString, value).map(cacheMap =>
+//            Redirect(navigator.nextPage(LocationId, mode)(new UserAnswers(cacheMap))))
+//
+
