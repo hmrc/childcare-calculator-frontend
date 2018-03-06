ea997a716a0364aa5a0b6d4113b0cd93ea54aeed
Vineet Tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2018-03-06 11:38:16 +0000

    CCALC-4087 code refactor

--- app/uk/gov/hmrc/childcarecalculatorfrontend/models/mappings/UserAnswerToHousehold.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/models/mappings/UserAnswerToHousehold.scala
@@ -187,7 +187,6 @@ class UserAnswerToHousehold @Inject()(appConfig: FrontendAppConfig, utils: Utils
     }
     vouchers.fold(Some(YesNoUnsureEnum.NO)) {
       case ChildcareConstants.Both | `whichParent` => Some(YesNoUnsureEnum.YES)
-      case ChildcareConstants.NotSure => Some(YesNoUnsureEnum.NOTSURE)
       case ChildcareConstants.Yes => Some(YesNoUnsureEnum.YES)
       case _ => Some(YesNoUnsureEnum.NO)
     }
