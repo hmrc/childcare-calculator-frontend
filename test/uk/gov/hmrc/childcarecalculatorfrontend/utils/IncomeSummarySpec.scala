78ea55f6adf6716b6cc92aadbd2695084f79f67a
juan <juanan_rp@hotmail.co.uk>
2018-03-21 12:34:18 +0000

    adding benefits and other income

--- test/uk/gov/hmrc/childcarecalculatorfrontend/utils/IncomeSummarySpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/utils/IncomeSummarySpec.scala
@@ -10,37 +10,71 @@ import uk.gov.hmrc.http.cache.client.CacheMap
 class IncomeSummarySpec extends PlaySpec with MockitoSugar with SpecBase {
   "Your Income Summary" should {
     "Handle a single parent journey" when {
-      "Include your income" in {
-        when(answers.parentEmploymentIncomeCY) thenReturn Some(BigDecimal(30))
+      "The income section" when {
+        "has an income" in {
+          when(answers.parentEmploymentIncomeCY) thenReturn Some(BigDecimal(30))
 
-        val result = incomeSummary.load(answers)
+          val result = incomeSummary.load(answers)
 
-        result.get("Your income") mustBe Some("30")
+          result.get("Your income") mustBe Some("30")
+        }
       }
 
-      "Include your pension payments a month" in {
-        when(answers.YouPaidPensionCY) thenReturn Some(true)
-        when(answers.howMuchYouPayPension) thenReturn Some(BigDecimal(300))
+      "The pension section" when {
+        "has a pension" in {
+          when(answers.YouPaidPensionCY) thenReturn Some(true)
+          when(answers.howMuchYouPayPension) thenReturn Some(BigDecimal(300))
 
-        val result = incomeSummary.load(answers)
+          val result = incomeSummary.load(answers)
 
-        result.get("Your pension payments a month") mustBe Some("300")
+          result.get("Your pension payments a month") mustBe Some("300")
+        }
+
+        "does not have a pension" in {
+          when(answers.YouPaidPensionCY) thenReturn Some(false)
+
+          val result = incomeSummary.load(answers)
+
+          result.get("Paid into a pension") mustBe Some("No")
+        }
       }
 
-      "Include that you don't have pension" in {
-        when(answers.YouPaidPensionCY) thenReturn Some(false)
+      "Your other income section" when {
+        "has another income" in {
+          when(answers.yourOtherIncomeThisYear) thenReturn Some(true)
+          when(answers.yourOtherIncomeAmountCY) thenReturn Some(BigDecimal(300))
 
-        val result = incomeSummary.load(answers)
+          val result = incomeSummary.load(answers)
 
-        result.get("Paid into a pension") mustBe Some("No")
+          result.get("Your other income") mustBe Some("300")
+        }
+
+        "does not have another income" in {
+          when(answers.yourOtherIncomeThisYear) thenReturn Some(false)
+
+          val result = incomeSummary.load(answers)
+
+          result.get("Other income") mustBe Some("No")
+        }
       }
 
-      "Include your other income" in {
-        when(answers.yourOtherIncomeAmountCY) thenReturn Some(BigDecimal(300))
+      "Your benefits section" when {
+        "has benefits" in {
+          when(answers.youAnyTheseBenefits) thenReturn Some(true)
+          when(answers.youBenefitsIncomeCY) thenReturn Some(BigDecimal(500))
 
-        val result = incomeSummary.load(answers)
+          val result = incomeSummary.load(answers)
 
-        result.get("Your other income") mustBe Some("300")
+          result.get("Your benefits income") mustBe Some("500")
+        }
+
+        "does not have benefits" in {
+          when(answers.youAnyTheseBenefits) thenReturn Some(false)
+
+          val result = incomeSummary.load(answers)
+
+          result.get("Income from benefits") mustBe Some("No")
+        }
       }
     }
   }
@@ -48,22 +82,24 @@ class IncomeSummarySpec extends PlaySpec with MockitoSugar with SpecBase {
 
   val incomeSummary = new IncomeSummary()
   val answers = spy(userAnswers())
+
   def userAnswers(answers: (String, JsValue)*): UserAnswers = new UserAnswers(CacheMap("", Map(answers: _*)))
 
   class IncomeSummary {
-    def load(userAnswers: UserAnswers) : Map[String,String] = {
-      val result : Map[String,String] = Map()
-      val parentIncome = loadParentIncome(userAnswers, _ : Map[String,String])
-      val parentPension = loadHowMuchYouPayPension(userAnswers, _ : Map[String, String])
-      val parentOtherIncome = loadYourOtherIncome(userAnswers, _ : Map[String,String])
-      (parentIncome andThen parentPension andThen parentOtherIncome)(result)
+    def load(userAnswers: UserAnswers): Map[String, String] = {
+      val result: Map[String, String] = Map()
+      val parentIncome = loadParentIncome(userAnswers, _: Map[String, String])
+      val parentPension = loadHowMuchYouPayPension(userAnswers, _: Map[String, String])
+      val parentOtherIncome = loadYourOtherIncome(userAnswers, _: Map[String, String])
+      val parentBenefitsIncome = loadYourBenefitsIncome(userAnswers, _: Map[String, String])
+      (parentIncome andThen parentPension andThen parentOtherIncome andThen parentBenefitsIncome) (result)
     }
 
     private def loadParentIncome(userAnswers: UserAnswers, result: Map[String, String]) = {
       userAnswers.parentEmploymentIncomeCY.foldLeft(result)((result, income) => result + ("Your income" -> income.toString()))
     }
 
-    private def loadHowMuchYouPayPension(userAnswers: UserAnswers, result: Map[String,String]) = {
+    private def loadHowMuchYouPayPension(userAnswers: UserAnswers, result: Map[String, String]) = {
       userAnswers.YouPaidPensionCY match {
         case Some(paysPension) => {
           if (paysPension) {
@@ -77,8 +113,32 @@ class IncomeSummarySpec extends PlaySpec with MockitoSugar with SpecBase {
       }
     }
 
-    private def loadYourOtherIncome(userAnswers: UserAnswers, result: Map[String,String]) = {
-      userAnswers.yourOtherIncomeAmountCY.foldLeft(result)((result, income) => result + ("Your other income" -> income.toString()))
+    private def loadYourOtherIncome(userAnswers: UserAnswers, result: Map[String, String]) = {
+      userAnswers.yourOtherIncomeThisYear match {
+        case Some(hasOtherIncome) => {
+          if (hasOtherIncome) {
+            userAnswers.yourOtherIncomeAmountCY.foldLeft(result)((result, income) => result + ("Your other income" -> income.toString()))
+          }
+          else {
+            result + ("Other income" -> "No")
+          }
+        }
+        case _ => result
+      }
+    }
+
+    private def loadYourBenefitsIncome(userAnswers: UserAnswers, result: Map[String, String]) = {
+      userAnswers.youAnyTheseBenefits match {
+        case Some(hasBenefits) => {
+          if (hasBenefits) {
+            userAnswers.youBenefitsIncomeCY.foldLeft(result)((result, income) => result + ("Your benefits income" -> income.toString()))
+          }
+          else {
+            result + ("Income from benefits" -> "No")
+          }
+        }
+        case _ => result
+      }
     }
   }
 }
