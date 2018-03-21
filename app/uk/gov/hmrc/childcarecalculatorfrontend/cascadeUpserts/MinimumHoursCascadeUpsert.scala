fe02e340eb68a6d08264bfdcedba4dd666369687
Vineet Tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2018-03-21 14:56:41 +0000

    CCALC-4143 Add clearing logic for Childcarecost and ApproverProvider pages

--- app/uk/gov/hmrc/childcarecalculatorfrontend/cascadeUpserts/MinimumHoursCascadeUpsert.scala
+++ app/uk/gov/hmrc/childcarecalculatorfrontend/cascadeUpserts/MinimumHoursCascadeUpsert.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.childcarecalculatorfrontend.cascadeUpserts
 
 import javax.inject.Inject
 
-import play.api.libs.json.{JsString, JsValue}
+import play.api.libs.json.{JsBoolean, JsString, JsValue}
 import uk.gov.hmrc.childcarecalculatorfrontend.identifiers._
 import uk.gov.hmrc.childcarecalculatorfrontend.models.{YesNoNotYetEnum, YesNoUnsureEnum}
 import uk.gov.hmrc.childcarecalculatorfrontend.utils.ChildcareConstants._
@@ -26,30 +26,70 @@ import uk.gov.hmrc.childcarecalculatorfrontend.utils.SubCascadeUpsert
 import uk.gov.hmrc.http.cache.client.CacheMap
 
 class MinimumHoursCascadeUpsert @Inject()() extends SubCascadeUpsert {
-  lazy val no: String = YesNoUnsureEnum.NO.toString
-
+  lazy val No: String = YesNoNotYetEnum.NO.toString
 
   val funcMap: Map[String, (JsValue, CacheMap) => CacheMap]  =
     Map(LocationId.toString -> ((v, cm) => storeLocation(v, cm)),
-      ChildcareCostsId.toString -> ((v,cm) => storeChildcareCosts(v,cm))
+      ChildcareCostsId.toString -> ((v,cm) => storeChildcareCosts(v,cm)),
+        ApprovedProviderId.toString -> ((v,cm) => storeApprovedProvider(v,cm))
     )
 
   private def storeLocation(value: JsValue, cacheMap: CacheMap): CacheMap = {
     val mapToStore = if (value == JsString(northernIreland)) {
       cacheMap copy (data = cacheMap.data - ChildAgedTwoId.toString)
-    } else
+    } else {
       cacheMap
+    }
 
     store(LocationId.toString, value, mapToStore)
   }
 
   private def storeChildcareCosts(value: JsValue, cacheMap: CacheMap): CacheMap = {
-    val mapToStore = if (value == JsString(no)) {
-      cacheMap copy (data = cacheMap.data - ApprovedProviderId.toString)
-    } else
-      cacheMap
+
+    val locationValue = cacheMap.data.getOrElse(LocationId.toString, JsString(England))
+    val childAgedTwoValue = cacheMap.data.getOrElse(ChildAgedTwoId.toString, JsBoolean(false))
+    val childAgedThreeOrFourValue = cacheMap.data.getOrElse(ChildAgedThreeOrFourId.toString, JsBoolean(false))
+
+    val existingChildCareCostValue = cacheMap.data.get(ChildcareCostsId.toString)
+
+    val mapToStore = value match {
+        case JsString(No) if !existingChildCareCostValue.contains(JsString(NO)) => {
+          cacheMap copy (data = Map(LocationId.toString -> locationValue,
+            ChildAgedTwoId.toString -> childAgedTwoValue,
+            ChildAgedThreeOrFourId.toString -> childAgedThreeOrFourValue))
+        }
+        case _ => {
+          cacheMap
+        }
+      }
 
     store(ChildcareCostsId.toString, value, mapToStore)
+  }
+
+  private def storeApprovedProvider(value: JsValue, cacheMap: CacheMap): CacheMap = {
+
+    val locationValue = cacheMap.data.getOrElse(LocationId.toString, JsString(England))
+    val childAgedTwoValue = cacheMap.data.getOrElse(ChildAgedTwoId.toString, JsBoolean(false))
+    val childAgedThreeOrFourValue = cacheMap.data.getOrElse(ChildAgedThreeOrFourId.toString, JsBoolean(false))
+    val childCareCostValue = cacheMap.data.getOrElse(ChildcareCostsId.toString, JsString(No))
+
+    val existingApprovedProviderValue = cacheMap.data.get(ApprovedProviderId.toString)
+
+    val NO: String = YesNoUnsureEnum.NO.toString
+
+    val mapToStore = value match {
+      case JsString(NO) if !existingApprovedProviderValue.contains(JsString(NO)) => {
+        cacheMap copy (data = Map(LocationId.toString -> locationValue,
+          ChildAgedTwoId.toString -> childAgedTwoValue,
+          ChildAgedThreeOrFourId.toString -> childAgedThreeOrFourValue,
+          ChildcareCostsId.toString -> childCareCostValue))
+      }
+      case _ => {
+        cacheMap
+      }
+    }
+
+    store(ApprovedProviderId.toString, value, mapToStore)
   }
 
 }
--- test/uk/gov/hmrc/childcarecalculatorfrontend/cascadeUpserts/MinimumHoursCascadeUpsertSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/cascadeUpserts/MinimumHoursCascadeUpsertSpec.scala
@@ -16,15 +16,17 @@
 
 package uk.gov.hmrc.childcarecalculatorfrontend.cascadeUpserts
 
-import play.api.libs.json.{JsBoolean, JsString}
+import org.joda.time.LocalDate
+import play.api.libs.json.{JsBoolean, JsNumber, JsString, Json}
 import uk.gov.hmrc.childcarecalculatorfrontend.identifiers._
-import uk.gov.hmrc.childcarecalculatorfrontend.models.YesNoUnsureEnum
+import uk.gov.hmrc.childcarecalculatorfrontend.models.{SelfEmployedOrApprenticeOrNeitherEnum, YesNoNotYetEnum, YesNoUnsureEnum, YouPartnerBothEnum}
 import uk.gov.hmrc.childcarecalculatorfrontend.utils.ChildcareConstants._
 import uk.gov.hmrc.childcarecalculatorfrontend.{CascadeUpsertBase, SpecBase}
 import uk.gov.hmrc.http.cache.client.CacheMap
 
 class MinimumHoursCascadeUpsertSpec extends SpecBase with CascadeUpsertBase {
-  lazy val no: String = YesNoUnsureEnum.NO.toString
+  lazy val no: String = YesNoNotYetEnum.NO.toString
+  lazy val No: String = YesNoUnsureEnum.NO.toString
 
   "MinimumHoursCascadeUpsert" when {
 
@@ -50,12 +52,97 @@ class MinimumHoursCascadeUpsertSpec extends SpecBase with CascadeUpsertBase {
     }
 
     "saving childcareCosts " must {
-      "remove the approvedProvider data when childcareCosts is 'no'" in {
-        val originalCacheMap = new CacheMap("id", Map(ApprovedProviderId.toString -> JsString(yes)))
+      "save the page data when user access the page first time and selects no" in {
+        val originalCacheMap = new CacheMap("id", Map(LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true)))
 
         val result = cascadeUpsert(ChildcareCostsId.toString, no, originalCacheMap)
         result.data mustBe Map(
+          ChildcareCostsId.toString -> JsString(no),
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true))
+      }
+
+      "remove all the data for subsequent pages the when user changes the selection from yes to no" in {
+        val originalCacheMap = new CacheMap("id", Map(
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
+          ChildcareCostsId.toString -> JsString(yes),
+          ApprovedProviderId.toString -> JsString(yes),
+          DoYouLiveWithPartnerId.toString -> JsBoolean(false),
+          WhoIsInPaidEmploymentId.toString -> JsString(partner), PartnerWorkHoursId.toString -> JsString("12"),
+          HasYourPartnersTaxCodeBeenAdjustedId.toString -> JsString(yes), DoYouKnowYourPartnersAdjustedTaxCodeId.toString -> JsBoolean(true),
+          WhatIsYourPartnersTaxCodeId.toString -> JsString("1100L"), PartnerChildcareVouchersId.toString -> JsString("yes"),
+          WhoGetsBenefitsId.toString -> JsString("you"), YourPartnersAgeId.toString -> JsString("under18"),
+          PartnerMinimumEarningsId.toString -> JsBoolean(true),
+          PartnerSelfEmployedOrApprenticeId.toString -> JsString(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString),
+          PartnerMaximumEarningsId.toString -> JsBoolean(true),
+          BothStatutoryPayId.toString -> JsBoolean(true),
+          WhoGotStatutoryPayId.toString -> JsString(YouPartnerBothEnum.PARTNER.toString),
+          PartnerStatutoryPayTypeId.toString -> JsString("maternity"),
+          PartnerStatutoryStartDateId.toString -> Json.toJson(new LocalDate(2017, 2, 1)),
+          PartnerStatutoryWeeksId.toString -> JsNumber(200),
+          PartnerStatutoryPayBeforeTaxId.toString -> JsString("true"),
+          PartnerStatutoryPayPerWeekId.toString -> JsNumber(BigDecimal(200))))
+
+        val result = cascadeUpsert(ChildcareCostsId.toString, no, originalCacheMap)
+        result.data mustBe Map(
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
           ChildcareCostsId.toString -> JsString(no))
+      }
+    }
+
+    "saving ApprovedProvider " must {
+      "save the page data when user access the page first time and selects no" in {
+        val originalCacheMap = new CacheMap("id", Map(LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
+          ChildcareCostsId.toString -> JsString(yes)))
+
+        val result = cascadeUpsert(ApprovedProviderId.toString, No, originalCacheMap)
+        result.data mustBe Map(
+          ApprovedProviderId.toString -> JsString(No),
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
+          ChildcareCostsId.toString -> JsString(yes))
+      }
+
+      "remove all the data for subsequent pages the when user changes the selection from yes to no" in {
+        val originalCacheMap = new CacheMap("id", Map(
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
+          ChildcareCostsId.toString -> JsString(yes),
+          ApprovedProviderId.toString -> JsString(YesNoUnsureEnum.YES.toString),
+          DoYouLiveWithPartnerId.toString -> JsBoolean(false),
+          WhoIsInPaidEmploymentId.toString -> JsString(partner), PartnerWorkHoursId.toString -> JsString("12"),
+          HasYourPartnersTaxCodeBeenAdjustedId.toString -> JsString(yes), DoYouKnowYourPartnersAdjustedTaxCodeId.toString -> JsBoolean(true),
+          WhatIsYourPartnersTaxCodeId.toString -> JsString("1100L"), PartnerChildcareVouchersId.toString -> JsString("yes"),
+          WhoGetsBenefitsId.toString -> JsString("you"), YourPartnersAgeId.toString -> JsString("under18"),
+          PartnerMinimumEarningsId.toString -> JsBoolean(true),
+          PartnerSelfEmployedOrApprenticeId.toString -> JsString(SelfEmployedOrApprenticeOrNeitherEnum.SELFEMPLOYED.toString),
+          PartnerMaximumEarningsId.toString -> JsBoolean(true),
+          BothStatutoryPayId.toString -> JsBoolean(true),
+          WhoGotStatutoryPayId.toString -> JsString(YouPartnerBothEnum.PARTNER.toString),
+          PartnerStatutoryPayTypeId.toString -> JsString("maternity"),
+          PartnerStatutoryStartDateId.toString -> Json.toJson(new LocalDate(2017, 2, 1)),
+          PartnerStatutoryWeeksId.toString -> JsNumber(200),
+          PartnerStatutoryPayBeforeTaxId.toString -> JsString("true"),
+          PartnerStatutoryPayPerWeekId.toString -> JsNumber(BigDecimal(200))))
+
+        val result = cascadeUpsert(ApprovedProviderId.toString, No, originalCacheMap)
+        result.data mustBe Map(
+          LocationId.toString -> JsString(England),
+          ChildAgedTwoId.toString -> JsBoolean(false),
+          ChildAgedThreeOrFourId.toString -> JsBoolean(true),
+          ApprovedProviderId.toString -> JsString(No),
+          ChildcareCostsId.toString -> JsString(yes))
       }
     }
   }
