6cb8100740f0324fe8da9a2a869b128c00c68d01
abdi1994 <abdi1994@hotmail.com>
2018-08-02 11:43:50 +0100

    Change to match DL-806

--- public/javascript/ccc.js
+++ public/javascript/ccc.js
@@ -3,18 +3,18 @@ window.onload = function() {
     $("#submit").click(function(){
         if ($('input[type="radio"]').is(":checked")) {
             var checkedValue = $('input[type="radio"]:checked').val();
-            ga('send', 'event', 'decision', 'submit', getCheckedGAValue(checkedValue));
+            ga('send', 'event', 'radio - select', 'Do you get tax credits or Universal Credit?', getCheckedGAValue(checkedValue));
         }
     });
 
 
     function getCheckedGAValue (value) {
-        if(value === "true") {
-            return "yes"
-        } else if (value === "false") {
-            return "no"
-        } else {
-            return value
+        if(value === "tc") {
+            return "Tax credits (includes Working and Child Tax Credit)"
+        } else if (value === "uc") {
+            return "Universal Credit"
+        } else if (value === "none") {
+            return "None of these"
         }
     }
 
