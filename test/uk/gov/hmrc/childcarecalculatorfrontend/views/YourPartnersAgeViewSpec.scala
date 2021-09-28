6d5037b60a13c259af0ab3a7924b8ead95e2288a
Pankaj Khare <12529426+pankaj2453@users.noreply.github.com>
2021-09-28 18:19:36 +0100

    DL-6243 yourPartnersAge.scala.html

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/YourPartnersAgeViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/YourPartnersAgeViewSpec.scala
@@ -19,10 +19,10 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 import play.api.data.Form
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.YourPartnersAgeForm
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
-import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.ViewBehaviours
+import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.NewViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.yourPartnersAge
 
-class YourPartnersAgeViewSpec extends ViewBehaviours {
+class YourPartnersAgeViewSpec extends NewViewBehaviours {
 
   val view = app.injector.instanceOf[yourPartnersAge]
   val messageKeyPrefix = "yourPartnersAge"
