182c987b3cc9e8c12b29d20cdab270e9fd86fd8e
JoshMiernik <josh.miernik@digital.hmrc.gov.uk>
2021-10-07 15:25:29 +0100

    DL-6266 areYouInPaidWork.scala

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/AreYouInPaidWorkViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/AreYouInPaidWorkViewSpec.scala
@@ -19,12 +19,13 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 import play.api.data.Form
 import uk.gov.hmrc.childcarecalculatorfrontend.controllers.routes
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.BooleanForm
-import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.YesNoViewBehaviours
+import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.NewYesNoViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.areYouInPaidWork
 
-class AreYouInPaidWorkViewSpec extends YesNoViewBehaviours {
+class AreYouInPaidWorkViewSpec extends NewYesNoViewBehaviours {
 
+  override val form = BooleanForm()
   val messageKeyPrefix = "areYouInPaidWork"
   val view = application.injector.instanceOf[areYouInPaidWork]
 
