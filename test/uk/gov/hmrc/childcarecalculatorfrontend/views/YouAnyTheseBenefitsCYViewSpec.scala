1a30f84e17147f86be1e8396113bfdfe1c77ac04
JoshMiernik <63236120+JoshMiernik@users.noreply.github.com>
2021-10-14 14:30:03 +0100

    Revert "DL-6283 - UI upgrade for page 'partnerAnyOtherIncomeThisYear'"

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/YouAnyTheseBenefitsCYViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/YouAnyTheseBenefitsCYViewSpec.scala
@@ -19,14 +19,13 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 import play.api.data.Form
 import uk.gov.hmrc.childcarecalculatorfrontend.controllers.routes
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.BooleanForm
-import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.NewYesNoViewBehaviours
+import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.YesNoViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
 import uk.gov.hmrc.childcarecalculatorfrontend.utils.TaxYearInfo
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.youAnyTheseBenefitsCY
 
-class YouAnyTheseBenefitsCYViewSpec extends NewYesNoViewBehaviours {
+class YouAnyTheseBenefitsCYViewSpec extends YesNoViewBehaviours {
 
-  override val form = BooleanForm()
   val view = app.injector.instanceOf[youAnyTheseBenefitsCY]
   val taxYearInfo = new TaxYearInfo
 
