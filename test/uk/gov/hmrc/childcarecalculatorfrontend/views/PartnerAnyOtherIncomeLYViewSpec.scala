028521782bcf903bf2f7eab9f1723144cb792900
Pankaj Khare <12529426+pankaj2453@users.noreply.github.com>
2021-10-18 12:59:37 +0100

    DL-6293 partnerAnyOtherIncomeLY.scala.html

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/PartnerAnyOtherIncomeLYViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/PartnerAnyOtherIncomeLYViewSpec.scala
@@ -19,13 +19,14 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 import play.api.data.Form
 import uk.gov.hmrc.childcarecalculatorfrontend.controllers.routes
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.BooleanForm
-import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.YesNoViewBehaviours
+import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.NewYesNoViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
 import uk.gov.hmrc.childcarecalculatorfrontend.utils.TaxYearInfo
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.partnerAnyOtherIncomeLY
 
-class PartnerAnyOtherIncomeLYViewSpec extends YesNoViewBehaviours {
+class PartnerAnyOtherIncomeLYViewSpec extends NewYesNoViewBehaviours {
 
+  override val form = BooleanForm()
   val taxYearInfo = new TaxYearInfo
   val view = app.injector.instanceOf[partnerAnyOtherIncomeLY]
 
