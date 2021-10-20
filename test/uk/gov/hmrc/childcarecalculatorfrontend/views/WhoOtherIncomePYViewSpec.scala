91974736fd2962426f3d94d55ed7dda64a4a82e5
AhmadNaeemQA <Ahmadzeya.Naeem@digital.hmrc.gov.uk>
2021-10-20 11:43:33 +0100

    DL-6301 - WhoOtherIncomePYForm

--- test/uk/gov/hmrc/childcarecalculatorfrontend/views/WhoOtherIncomePYViewSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/views/WhoOtherIncomePYViewSpec.scala
@@ -19,10 +19,10 @@ package uk.gov.hmrc.childcarecalculatorfrontend.views
 import play.api.data.Form
 import uk.gov.hmrc.childcarecalculatorfrontend.forms.WhoOtherIncomePYForm
 import uk.gov.hmrc.childcarecalculatorfrontend.models.NormalMode
-import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.ViewBehaviours
+import uk.gov.hmrc.childcarecalculatorfrontend.views.behaviours.NewViewBehaviours
 import uk.gov.hmrc.childcarecalculatorfrontend.views.html.whoOtherIncomePY
 
-class WhoOtherIncomePYViewSpec extends ViewBehaviours {
+class WhoOtherIncomePYViewSpec extends NewViewBehaviours {
 
   val view = app.injector.instanceOf[whoOtherIncomePY]
   val messageKeyPrefix = "whoOtherIncomePY"
