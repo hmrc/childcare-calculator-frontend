7ec07c2028a003d89af25d16868ed945abc787e6
Divya Prabha Manimaran <179695051+DivyaManimaran-hmrc@users.noreply.github.com>
2024-12-19 14:26:17 +0000

    DL-14600: Removal of TCSchemeInEligibilityMsgBuilder as it is no more used in the code. (#692)
    Co-authored-by: DivyaManimaran_hmrc <179695051+DivyaManimaran_hmrc@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import play.sbt.PlayImport.ws
 
 object AppDependencies {
 
-  val bootstrapFrontendVersion = "9.2.0"
+  val bootstrapFrontendVersion = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"         % bootstrapFrontendVersion,
-    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                 % "2.2.0",
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"         % "10.6.0",
+    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                 % "2.3.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"         % "11.8.0",
     "uk.gov.hmrc"        %% "tax-year"                           % "5.0.0"
   )
 
