3d1bec44c5ffdfbbf320c3959ddff00b0c9865d5
kwame-f-manso-acc <171707914+kwame-f-manso-acc@users.noreply.github.com>
2025-02-07 15:37:23 +0000

    DL-15367-v2: Resolved conflict when cherry picking commit 287e704d47693648280200107ad8034db7b44076 and 50381b0b33c902b5948db18ba4bc64147665227e

--- test/uk/gov/hmrc/childcarecalculatorfrontend/navigation/IncomeInfoNavigationSpec.scala
+++ test/uk/gov/hmrc/childcarecalculatorfrontend/navigation/IncomeInfoNavigationSpec.scala
@@ -79,33 +79,4 @@ class IncomeInfoNavigationSpec extends SpecBase with MockitoSugar with OptionVal
 
   }
 
-  "Previous Year Income Route Navigation" when {
-
-    "in Normal mode" must {
-      "NextPageUrlPY" must {
-        "return BothPaidWorkPY page when parent/partner/both in paid work and lives with partner" in {
-          val answers = spy(userAnswers())
-          when(answers.doYouLiveWithPartner) thenReturn Some(true)
-          when(answers.whoIsInPaidEmployment) thenReturn Some(you) thenReturn Some(partner) thenReturn Some(both)
-
-          navigator.nextPage(BothIncomeInfoPYId, NormalMode).value(answers) mustBe
-            routes.BothGetSameIncomePreviousYearController.onPageLoad(NormalMode)
-          navigator.nextPage(BothIncomeInfoPYId, NormalMode).value(answers) mustBe
-            routes.BothGetSameIncomePreviousYearController.onPageLoad(NormalMode)
-          navigator.nextPage(BothIncomeInfoPYId, NormalMode).value(answers) mustBe
-            routes.BothGetSameIncomePreviousYearController.onPageLoad(NormalMode)
-        }
-
-        "return sessionExpired page when user lives with partner and value is not present" in {
-          val answers = spy(userAnswers())
-          when(answers.doYouLiveWithPartner) thenReturn None
-
-          navigator.nextPage(BothIncomeInfoPYId, NormalMode).value(answers) mustBe
-            routes.SessionExpiredController.onPageLoad
-        }
-
-      }
-    }
-
-  }
 }
