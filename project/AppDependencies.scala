289ccca5ee31ba475c78cc1acc44c6b51555b030
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2025-06-11 12:00:51 +0100

    Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import play.sbt.PlayImport.ws
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.13.0"
   val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % "2.6.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.0.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.2.0",
     "uk.gov.hmrc"       %% "tax-year"                         % "6.0.0"
   )
 
